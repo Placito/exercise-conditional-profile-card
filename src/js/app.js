6d1fac0fa1bf1b6598c763033c949374aec757b5
Mariana <101410421+Placito@users.noreply.github.com>
2023-06-26 22:19:04 +0000

    update social media icons

--- src/js/app.js
+++ src/js/app.js
@@ -45,10 +45,26 @@ function render(variables = {}) {
               ? true
               : "position-right"
           }"> 
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${
+              variables.twitter === null
+                ? "https://twitter.com/4GeeksAcademy"
+                : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github === null
+                ? "https://github.com/4geeksacademy"
+                : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a  href="${
+              variables.linkedin === null
+                ? "https://linkedin.com/4geeksacademy"
+                : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram === null
+                ? "https://instagram.com/4geeksacademy"
+                : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
